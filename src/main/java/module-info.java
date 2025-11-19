017d5ba8ef9cb17092f0a5df3deabb259ba360f8
jackross <jackross@localhost>
2025-11-19 18:10:19 +0100

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module it.unisa.diem.oop.projectlibraryg13 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens it.unisa.diem.oop.projectlibraryg13 to javafx.fxml;
+    exports it.unisa.diem.oop.projectlibraryg13;
+}
