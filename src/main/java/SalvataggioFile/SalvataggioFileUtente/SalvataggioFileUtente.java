1a9b993919ced2437be6985d1575a2f8cbb36f0d
grossino1 <g.rossino1@studenti.unisa.it>
2025-12-04 11:11:19 +0100

    Suddivisione finale package

---
+++ src/main/java/SalvataggioFile/SalvataggioFileUtente/SalvataggioFileUtente.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SalvataggioFile.SalvataggioFileUtente;
+
+import GestioneUtente.Utente;
+
+/**
+ *
+ * @author jackross
+ */
+public class SalvataggioFileUtente {
+    
+     public void salva(Utente dati, String filename){
+               
+    }
+    
+    public static Utente carica(String filename){
+        return null;
+    }
+}
