57ad93e82613e266fbb1236f1f50c6d5c196cdaa
ciacuzzo5-ux <c.iacuzzo5@studenti.unisa.it>
2025-12-10 10:16:30 +0100

    aggiunta junit

---
+++ src/test/java/UtenteTest/UtenteTest.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UtenteTest;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author chiara
+ */
+public class UtenteTest {
+    
+}
