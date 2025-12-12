7b4f76aca4fba393b22c732bb46519099e24a8e2
grossino1 <g.rossino1@studenti.unisa.it>
2025-12-12 09:29:07 +0100

    BibliotecarioTest + SalvataggioFileBibliotecario

---
+++ src/main/java/SalvataggioFile/SalvataggioFileAutenticazione/SalvataggioFileBibliotecario.java
@@ -0,0 +1,72 @@
+package SalvataggioFile.SalvataggioFileAutenticazione;
+
+import Autenticazione.Bibliotecario;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * @class SalvataggioFileBibliotecario
+ * @brief Gestisce la persistenza su file degli oggetti di tipo Bibliotecario.
+ *
+ * Questa classe di utilità si occupa di serializzare (salvare) e deserializzare (caricare)
+ * le istanze della classe @ref Bibliotecario su file binari.
+ *
+ * @see Autenticazione.Bibliotecario
+ * @see java.io.ObjectOutputStream
+ * @see java.io.ObjectInputStream
+ *
+ * @author grossino1
+ * @version 1.0
+ */
+
+public class SalvataggioFileBibliotecario {
+    
+    private static final String FILE_PATH = "us.bin";
+    
+    /**
+     * @brief Carica un oggetto Bibliotecario da un file binario.
+     *
+     * Tenta di leggere e deserializzare un oggetto Bibliotecario dal percorso specificato.
+     *
+     * @pre filename != null && !filename.isEmpty()
+     * @post Restituisce un oggetto Bibliotecario valido se la lettura ha successo.
+     * @post Restituisce null se il file non esiste o la deserializzazione fallisce.
+     *
+     * @return L'istanza di Bibliotecario recuperata, oppure null in caso di errore.
+     */
+    
+    public static Bibliotecario carica() throws IOException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH))) {
+            return (Bibliotecario) ois.readObject();
+        } catch (FileNotFoundException e) {
+            System.err.println("Errore: File credenziali 'us.bin' non trovato.");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Errore durante la lettura del file credenziali: " + e.getMessage());
+        }
+        return null;
+    }
+    
+    /**
+     * @brief Salva lo stato di un oggetto Bibliotecario su un file binario.
+     *
+     * Serializza l'oggetto passato come parametro e lo scrive nel percorso specificato.
+     * Se il file esiste già, viene sovrascritto.
+     *
+     * @pre dati != null (L'oggetto da salvare non può essere nullo).
+     * @post Viene creato o aggiornato un file contenente i dati dei libri.
+     *
+     * @param[in] dati: L'oggetto Bibliotecario da serializzare.
+     */
+    public static void salva(Bibliotecario dati) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH))) {
+            oos.writeObject(dati);
+            System.out.println("Credenziali salvate correttamente in " + FILE_PATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
