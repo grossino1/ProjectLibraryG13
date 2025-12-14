696a091606e111f6e2c4268ab6d57842ff3a088c
grossino1 <g.rossino1@studenti.unisa.it>
2025-12-14 15:02:15 +0100

    Aggiunta .jar

---
+++ src/main/java/it/unisa/diem/oop/projectlibraryg13/Launcher.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.diem.oop.projectlibraryg13;
+
+/**
+ *
+ * @author jackross
+ */
+public class Launcher {
+    
+    public static void main(String[] args) {
+        // Sostituisci 'Main' con il nome della tua classe principale che estende Application
+        App.main(args);
+    }
+}
